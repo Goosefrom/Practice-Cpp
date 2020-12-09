c28bdadb79720fdfbd49237d4ebfe9b7f750517c
Goosefrom <sanchoskorch765@gmail.com>
2020-12-09 15:28:36 +0200

    labs on Cpp

---
+++ cpa_lab_4_2_5_(1)-A.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long number;
+	cout << "Enter number: ";
+	cin >> number;
+	cout << "Result: " << (1 + number) * number / 2;
+	return 0;
+}
+
+
+
+  
